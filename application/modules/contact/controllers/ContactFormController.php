a206e424fd71c808ebf97cb6656cbc4d37bae0cf
Anusch Athari <anusch@athari.de>
2012-08-23 22:45:24 +0200

    work on contact form

--- application/modules/contact/controllers/ContactFormController.php
+++ application/modules/contact/controllers/ContactFormController.php
@@ -4,24 +4,43 @@ class Contact_ContactFormController extends JdLikesYou_Controller_Action
     public function readAction()
     {
     	$contactForm = new Contact_Form_Contact();
+    	
+    	if ($this->getRequest()->isPost())
+		{
+        	if ($contactForm->isValid($_POST))
+        	{
+            	$this->view->assign('flashMessages', array(
+        			array(
+					    'type' => Athari_Controller_Action_Helper_FlashMessenger::TYPE_SUCCESS,
+        				'text'	 => 'Nachricht erfolgreich verschickt.')
+        			)
+    			);
 
-    	if (!$this->getRequest()->isPost())
-    	{
-            $this->view->assign('contactForm', $contactForm);
-            return; 
-    	}
-    	
-    	if ($contactForm->isValid($_POST)) {
-    		// success!
-    	} else {
-    		// failure!
-    	}
-    	
+            	$mail = new Athari_Mail();
+        		$mail->addTo('hello@jdlikesyou.de');
+        		$mail->setSubject('jdlikesyou.de Contact Form');
+        		
+        		$body = 'Name: ' . PHP_EOL . $contactForm->getValue('name') . PHP_EOL . PHP_EOL  ;
+        		$body .= 'E-Mail: ' . PHP_EOL . $contactForm->getValue('email') . PHP_EOL . PHP_EOL ;
+        		$body .= 'Nachricht: ' . PHP_EOL . $contactForm->getValue('body') . PHP_EOL . PHP_EOL ;
+
+        		$mail->setBodyText($body);
+        		$mail->send();
+        	}else{
+            	$this->view->assign('flashMessages', array(
+        			array(
+					    'type' => Athari_Controller_Action_Helper_FlashMessenger::TYPE_ERROR,
+        				'text'	 => 'Bitte Eingaben überprüfen.')
+        			)
+    			);
+        	}
+		}
+
+
+    	$this->view->assign('contactForm', $contactForm);   	
     }
     
-    protected function processContactForm()
-    {
+    public function processAction(){
     	
     }
-    
 }
--- application/modules/contact/forms/Contact.php
+++ application/modules/contact/forms/Contact.php
@@ -7,15 +7,15 @@ class Contact_Form_Contact extends Zend_Form
 		$nameText->setLabel('Name');
 		$nameText->setRequired();
 		$nameText->addValidator(new Zend_Validate_StringLength(array('max' => 150)));
+		$nameText->setErrorMessages(array('Bitte den Namen überprüfen.'));	
 		$this->addElement($nameText);
-
 		
 		$emailText = new Zend_Form_Element_Text('email');
 		$emailText->setLabel('E-Mail');
 		$emailText->setRequired();
 		$emailText->addValidator('EmailAddress', true);
+		$emailText->setErrorMessages(array('Bitte die E-Mail-Adresse überprüfen.'));	
 		$this->addElement($emailText);
-
 		
 		$bodyTextArea = new Zend_Form_Element_Textarea('body');
 		$bodyTextArea->setLabel('Nachricht');
@@ -23,20 +23,11 @@ class Contact_Form_Contact extends Zend_Form
 		$bodyTextArea->setAttrib('cols', '40');
 		$bodyTextArea->setAttrib('rows', '8');
 		$bodyTextArea->addValidator(new Zend_Validate_StringLength(array('max' => 5000)));
+		$bodyTextArea->setErrorMessages(array('Bitte die Nachricht überprüfen.'));	
 		$this->addElement($bodyTextArea);
 		
 		$submitButton = new Zend_Form_Element_Submit('submit');
 		$submitButton->setLabel('Abschicken');
 		$this->addElement($submitButton);
-		
-		
-		foreach($this->getElements() as $element) {
-			$label = $element->getDecorator('label');
-			if($label) {
-				$label->setOption('requiredSuffix', '*');
-			}
-		}
-		
-
 	}
 }
